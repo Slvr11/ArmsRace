53d93a83b98462ab4389e5af4c4a924567a10426
Slvr99 <bsawii@gmail.com>
2019-04-27 19:26:46 -0400

    Add project files.

---
+++ ArmsRace/ArmsRace.cs
@@ -0,0 +1,16 @@
+﻿using InfinityScript;
+
+public class armsRaceMsg : BaseScript
+{
+    public armsRaceMsg()
+    {
+        HudElem info = HudElem.CreateServerFontString(HudElem.Fonts.HudBig, 0.8f);
+        info.SetPoint("CENTER", "CENTER");
+        info.HideWhenInMenu = true;
+        info.HideIn3rdPerson = false;
+        info.HideWhenInDemo = false;
+        info.HideWhenDead = true;
+        info.Archived = true;
+        info.SetText("^1You have 10 seconds to get a kill!");
+    }
+}
